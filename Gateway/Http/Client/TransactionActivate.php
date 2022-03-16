2e45fe496d6a7881484896da2a07b0e7bf23d3ab
Martin Sternelius <martin.sternelius@nordicwebteam.se>
2022-03-16 16:00:06 +0100

    [BC-38] Improved error handling for when capture is attempted before payment has received an invoice number from Billmate's callback

--- Gateway/Http/Client/TransactionActivate.php
+++ Gateway/Http/Client/TransactionActivate.php
@@ -3,6 +3,7 @@
 namespace Billmate\NwtBillmateCheckout\Gateway\Http\Client;
 
 use Billmate\NwtBillmateCheckout\Gateway\Validator\ResponseValidator;
+use Magento\Framework\Exception\PaymentException;
 
 class TransactionActivate extends AbstractTransaction
 {
@@ -12,6 +13,12 @@ class TransactionActivate extends AbstractTransaction
     public function process(array $data)
     {
         $invoiceNumber = $data[ResponseValidator::KEY_INVOICE_NUMBER];
+        if (!$invoiceNumber) {
+            throw new PaymentException(
+                __("Payment is missing a Billmate invoice number! Check for callback errors in Billmate's backend.")
+            );
+        }
+
         $credentials = $data['credentials'];
         $result = [];
         $response = $this->adapter->activatePayment($invoiceNumber, $credentials);
